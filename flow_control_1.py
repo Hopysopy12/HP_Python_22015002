a28e4ef366cf1d15ec3c1a2832182686ce99725c
Hopysopy12 <134638781+Hopysopy12@users.noreply.github.com>
2023-05-25 19:08:16 +0100

    Update flow_control_1.py

--- flow_control_1.py
+++ flow_control_1.py
@@ -1,1 +1,9 @@
-
+for x in range(1, 51): 
+    if x % 5 == 0: 
+        print("Girl")
+    elif x % 3 == 0:  
+        print("Boy")
+    elif x % 3 == 0 & x % 5 == 0:
+        print("couple")
+    else
+        print(x)
