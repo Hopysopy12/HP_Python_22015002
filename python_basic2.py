2845b65f7438c3d581ac18789d2e266ab4e77e1c
Hopysopy12 <134638781+Hopysopy12@users.noreply.github.com>
2023-05-25 19:27:00 +0100

    Update python_basic2.py

--- python_basic2.py
+++ python_basic2.py
@@ -1,1 +1,10 @@
+print("Swapper")
+a = 6
+b = 8
+print(f"The value in a = {a}\nThe Value in b = {b}")
+# Swapping from here
+a, b = b, a
 
+print("\n")
+
+print(f"The value in a = {a}\nThe Value in b = {c}")
