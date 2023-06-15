3781e6ecb1f06e8ad47f6a991873800a85fcd7a6
Hopysopy12 <134638781+Hopysopy12@users.noreply.github.com>
2023-06-15 16:40:01 +0100

    Update 1-triangle_shape.py

--- 1-triangle_shape.py
+++ 1-triangle_shape.py
@@ -1,1 +1,14 @@
+try: 
+   userIn = int(input("Enter : "))
 
+   if n%2!=0:
+      for i in range(userIn):
+          for j in range(i):
+              print(" ", end="")
+          for j in range(userIn-i):
+              print("*", end=" ")
+          print()
+   else:
+       print("Should be an odd number")
+except:
+    print("Enter number only")
